676c08641f3b95efbb894f311791eae86b7b2fd9
The Doom Lab <buchananlab@gmail.com>
2023-10-02 07:23:01 -0500

    adding plots and mgcfa

---
+++ R/mgcfa.R
@@ -0,0 +1,223 @@
+#' Multigroup Confirmatory Factor Analysis
+#'
+#' A function to calculate multigroup confirmatory
+#' factor analysis given a list of model constraints.
+#' Returns all models using \code{lavaan} package
+#' and tidy dataframes of relevant information.
+#'
+#' @param model A character string representing the
+#' overall model formatted in \code{lavaan} syntax
+#' @param data A dataframe that includes a grouping
+#' variable for multigroup analysis. Define \code{data}
+#' and \code{group} OR \code{sample.cov},
+#' \code{sample.mean}, and \code{sample.nobs}.
+#' @param sample.cov A sample variance-covariance
+#' matrix that includes row and/or column names that
+#' match the observed variables in the model. For
+#' multigroup analyses, you need to format this data
+#' as a list of variance-covariance matrices with
+#' names for each group.
+#' @param sample.mean The mean vector for observed
+#' variables. For multigroup analyses, you need to format
+#' this data as a list with names for each group.
+#' It is assumed they are in the same order as
+#' \code{sample}.cov.
+#' @param sample.nobs Number of observations for each
+#' group. For multigroup analyses, you need to format
+#' this data as a list with names for each group.
+#' It is assumed they are in the same order as
+#' \code{sample}.cov.
+#' @param group A character name of the group column
+#' included in the \code{data} argument.
+#' @param group.equal A vector of names of the
+#' constraints to test for multigroup analysis, in
+#' the order that they should be tested. See \code{lavaan}
+#' for possible options.
+#' @param group.partial A vector of constraints to relax
+#' for partial group models.
+#' @param conf.level The confidence interval for model
+#' coefficients.
+#' @param ... Other arguments to be included to configure
+#' the \code{cfa} function from \code{lavaan}. For
+#' example, you can include arguments for ordered
+#' models, clustering, sampling.weights, or estimators.
+#'
+#' @return A list of outputs from the multigroup
+#' procedure.
+#'
+#' \item{model_coef}{A tidy dataframe of model
+#' coefficients}
+#' \item{model_fit}{A tidy dataframe of model fit indices}
+#' \item{model.overall}{If a dataframe is provided,
+#' the overall CFA model with all groups combined is included}
+#' \item{model.GROUPS}{The individual group models}
+#' \item{model.configural}{The configural model that
+#' puts together both groups in the same model with
+#' no constraints}
+#' \item{model.CONSTRAINTS}{The model constraints provided
+#' in group.equal are converted into models with those
+#' increasing equality constraints}
+#'
+#' @keywords multigroup cfa, sem, lavaan
+#' @import lavaan dplyr tidyr
+#' @rdname mgcfa
+#' @export
+
+mgcfa <- function(model,
+                  data = NULL,
+                  sample.cov = NULL,
+                  sample.mean = NULL,
+                  sample.nobs = NULL,
+                  group = NULL,
+                  group.equal,
+                  group.partial = NULL,
+                  conf.level = conf.level,
+                  ...){
+
+  # Deal with missing information -------------------------------------------
+
+  if((is.null(data) | is.null(group)) |
+     (is.null(sample.cov) |
+      is.null(sample.mean) |
+      is.null(sample.nobs))){
+    stop("You must define the data and group OR the sample.cov, sample.mean, and sample.nobs")
+  }
+
+  # Do this if data ---------------------------------------------------------
+
+  if(!is.null(data)){
+
+    # group information
+    group_names <- unique(data[ , group])
+    data$group <- data[ , group]
+
+    # do the overall models
+    model.overall <- cfa(model = model,
+                         data = data,
+                         ...)
+
+    group_models <- list()
+    for (i in 1:length(group_names)){
+
+      group_models[[paste0("model.", group_names[i])]] <-
+        cfa(model = model,
+            data = subset(data, group == group_names[i]),
+            ...)
+    }
+
+    model.configural <- cfa(model = model,
+                            data = data,
+                            group = group,
+                            ...)
+
+    equal_models <- list()
+    # do steps they are interested in
+    for (i in 1:length(group.equal)){
+
+      equal_models[[paste0("model.", group.equal[i])]] <-
+        cfa(model = model,
+            data = data,
+            group = group,
+            group.equal = group.equal[1:i],
+            group.partial = group.partial,
+            ...)
+    }
+
+  } else {
+
+    group_names <- names(sample.cov)
+    names(sample.nobs) <- names(sample.mean) <- group_names
+
+    # do the overall models
+    model.overall <- NULL
+
+    group_models <- list()
+    for (i in 1:length(group_names)){
+
+      group_models[[paste0("model.", group_names[i])]] <-
+        cfa(model = model,
+            sample.nobs = sample.nobs[[group_names[i]]],
+            sample.mean = sample.mean[[group_names[i]]],
+            sample.cov = sample.cov[[group_names[i]]],
+            ...)
+    }
+
+    model.configural <- cfa(model = model,
+                            data = data,
+                            sample.nobs = sample.nobs,
+                            sample.mean = sample.mean,
+                            sample.cov = sample.cov,
+                            ...)
+
+    equal_models <- list()
+    # do steps they are interested in
+    for (i in 1:length(group.equal)){
+
+      equal_models[[paste0("model.", group.equal[i])]] <-
+        cfa(model = model,
+            sample.nobs = sample.nobs,
+            sample.mean = sample.mean,
+            sample.cov = sample.cov,
+            group.equal = group.equal[1:i],
+            group.partial = group.partial,
+            ...)
+    }
+
+  }
+
+  # Put together models -----------------------------------------------------
+  model_coef <- bind_rows(
+    tidy(model.overall, conf.level = conf.level) %>% mutate(model = "Overall"))
+
+  model_fit <- bind_rows(
+    glance(model.overall) %>% mutate(model = "Overall"))
+
+  for (i in 1:length(group_models)){
+    model_coef <- bind_rows(
+      model_coef,
+      tidy(group_models[[i]], conf.level = conf.level) %>%
+        mutate(model = group_names[i])
+    )
+
+    model_fit <- bind_rows(
+      model_fit,
+      glance(group_models[[i]]) %>%
+        mutate(model = group_names[i])
+    )
+  }
+
+  model_coef <- bind_rows(
+    model_coef,
+    tidy(model.configural, conf.level = conf.level) %>% mutate(model = "Configural")
+  )
+
+  model_fit <- bind_rows(
+    model_fit,
+    glance(model.configural) %>% mutate(model = "Configural")
+  )
+
+  for (i in 1:length(equal_models)){
+    model_coef <- bind_rows(
+      model_coef,
+      tidy(equal_models[[i]], conf.level = conf.level) %>%
+        mutate(model = group.equal[i])
+    )
+
+    model_fit <- bind_rows(
+      model_fit,
+      glance(equal_models[[i]]) %>%
+        mutate(model = group.equal[i])
+    )
+  }
+
+  return(list(
+    "model_coef" = model_coef,
+    "model_fit" = model_fit,
+    "model.overall" = model.overall,
+    "model.group1" = model.group1,
+    "model.group2" = model.group2,
+    "model.configural" = model.configural,
+    unlist(equal_models)
+  ))
+
+}
---
+++ R/plot_mi.R
@@ -0,0 +1,265 @@
+#' Visualization for Measurement Invariance
+#'
+#' A function to create visualizations for
+#' measurement invariance comparison between
+#' two groups. Please note: this function graphs
+#' two groups at a time - use the function several
+#' times if you have more than two groups.
+#'
+#' @param data_coef A tidy dataframe of the coefficients
+#' from \code{mgcfa} function in this package.
+#' @param model_step The model step you would like to
+#' plot using this function. Check the "Model" column
+#' in the coefficients to pick. Include this as a
+#' character vector.
+#' @param item_name The name of the item/variable
+#' you want to plot for comparison. Check out the
+#' "term" column to know your options. Usually
+#' observed variables from your model.
+#' @param x_limits The limits of the latent variable
+#' to plot on on the latent mean graph.
+#' @param y_limits The limits of the observed variable
+#' that you included in \code{item_name}.
+#' @param conf.level The confidence interval level you
+#' would like to graph.
+#' @param model_results The saved summary from \code{cfa}
+#' or \code{mgcfa} of the same model name you listed
+#' in \code{model_step}.
+#' @param lv_name The name of the latent variable you
+#' would like to plot. Ensure this name is the same
+#' as what is in the \code{data_coef}.
+#' @param plot_groups An optional variable to
+#' denote which two groups you would like to plot. Used
+#' when more than two groups are included in the output.
+#'
+#' @return A list of graphs created to visualize
+#' measurement invariance.
+#'
+#' \item{complete}{A ggplot2 object that includes all
+#' visualization stacked together for publication.}
+#' \item{intercept}{A ggplot2 object that includes the
+#' intercept section of the overall graph.}
+#' \item{mean}{A ggplot2 object that includes the
+#' latent means section of the overall graph.}
+#' \item{variance}{A ggplot2 object that includes the
+#' variance/residuals section of the overall graph.}
+#'
+#' @keywords multigroup cfa, sem, lavaan
+#' @import lavaan dplyr tidyr ggplot2
+#' @rdname mgcfa
+#' @export
+
+plot_mgcfa <- function(data_coef, # output from model_coef
+                       model_step, # which model
+                       item_name, # name of observed item
+                       x_limits = c(-1,1), # LV limits to graph
+                       y_limits, # Y min and max in data
+                       conf.level = .95, # what ci do you want
+                       model_results, # what model results do you want
+                       lv_name, # which latent is the observed variable on
+                       plot_groups = NULL
+){
+
+
+  # Deal with missing information  ------------------------------------------
+  if(missing(data_coef) |
+     missing(model_step) |
+     missing(item_name) |
+     missing(y_limits) |
+     missing(model_results) |
+     missing(lv_name)) {
+    stop("You must define data coefficients, model step, item name,
+         y limits, model results, and the name of the latent variable.")
+  }
+
+  # calculate cutoff
+  cutoff <- qt(p = (1-conf.level)/2,
+               df = sum(unlist(model_results@Data@nobs)),
+               lower.tail = F)
+
+  # get group variable
+  group_var <- model_results@Data@group
+  group_labels <- model_results@Data@group.label
+
+  # pick the first two if it's null
+  if(is.null(plot_groups)){
+    plot_groups <- group_labels[1:2]
+  }
+
+  # first get the data
+  graph.data <- data_coef %>% # put in tidy coefficients
+    filter(model == model_step) %>% # pick a model
+    filter(grepl(item_name, term)) %>%  # pick a question
+    mutate(group = factor(group, levels = names(table(data_coef$group)),
+                          labels = group_labels)) %>%
+    filter(group_labels %in% plot_groups)
+
+  # make ribbon data y = slope*x + intercept for ci for slopes
+  ribbondata <- bind_rows(
+    data.frame(
+      x = seq(from = x_limits[1] - 1,
+              to = x_limits[2] + 1,
+              by = .05),
+      group = unique(graph.data$group)[1]
+    ) %>%
+      mutate(ymin = (graph.data %>% filter(op == "=~") %>%
+                       slice_head() %>% pull(estimate) * x) -
+               (cutoff*graph.data %>% filter(op == "=~") %>%
+                  slice_head() %>% pull(std.error)) +
+               graph.data %>% filter(op == "~1") %>%
+               slice_head() %>% pull(estimate),
+             ymax = (graph.data %>% filter(op == "=~") %>%
+                       slice_head() %>% pull(estimate) * x) +
+               (cutoff*graph.data %>% filter(op == "=~") %>%
+                  slice_head() %>% pull(std.error)) +
+               graph.data %>% filter(op == "~1") %>%
+               slice_head() %>% pull(estimate)),
+    data.frame(
+      x = seq(from = x_limits[1] - 1,
+              to = x_limits[2] + 1,
+              by = .05),
+      group = unique(graph.data$group)[2]
+    ) %>%
+      mutate(ymin = (graph.data %>% filter(op == "=~") %>%
+                       slice_tail() %>% pull(estimate) * x) -
+               (cutoff*graph.data %>% filter(op == "=~") %>%
+                  slice_tail() %>% pull(std.error)) +
+               graph.data %>% filter(op == "~1") %>%
+               slice_tail() %>% pull(estimate),
+             ymax = (graph.data %>% filter(op == "=~") %>%
+                       slice_tail() %>% pull(estimate) * x) +
+               (cutoff*graph.data %>% filter(op == "=~") %>%
+                  slice_tail() %>% pull(std.error)) +
+               graph.data %>% filter(op == "~1") %>%
+               slice_tail() %>% pull(estimate))
+  )
+
+  # make point data to draw on the intercepts
+  pointdata <- data.frame(
+    x = c(0,0),
+    y = graph.data %>% filter(op == "~1") %>% pull(estimate),
+    group = graph.data %>% filter(op == "~1") %>% pull(group),
+    ymin = graph.data %>% filter(op == "~1") %>% pull(estimate) -
+      cutoff * graph.data %>% filter(op == "~1") %>% pull(std.error),
+    ymax = graph.data %>% filter(op == "~1") %>% pull(estimate) +
+      cutoff * graph.data %>% filter(op == "~1") %>% pull(std.error)
+  )
+
+  # make the line data to draw on the slopes
+  linedata <- data.frame(
+    slope = graph.data %>% filter(op == "=~") %>% pull(estimate),
+    intercept = graph.data %>% filter(op == "~1") %>% pull(estimate),
+    group = graph.data %>% filter(op == "~1") %>% pull(group)
+  )
+
+  # make the distributions for the residuals
+  violindata <- data.frame(
+    y = c(rnorm(n = 1000,
+                mean = graph.data %>% filter(op == "~~") %>%
+                  slice_head() %>% pull(estimate),
+                sd = graph.data %>% filter(op == "~~") %>%
+                  slice_head() %>% pull(std.error)),
+          rnorm(n = 1000,
+                mean = graph.data %>% filter(op == "~~") %>%
+                  slice_tail() %>% pull(estimate),
+                sd = graph.data %>% filter(op == "~~") %>%
+                  slice_tail() %>% pull(std.error))),
+    group = c(rep(graph.data %>% filter(op == "~~") %>%
+                    slice_head() %>% pull(group), 1000),
+              rep(graph.data %>% filter(op == "~~") %>%
+                    slice_tail() %>% pull(group), 1000)),
+    x = 1
+  )
+
+  # make the latent mean data for right panel
+  latent_means <- lavPredict(model_results,
+                             type = "lv",
+                             label = TRUE,
+                             assemble = TRUE,
+                             append.data = TRUE)
+
+  latent_means$lv <- latent_means[ , lv_name]
+  latent_means$group <- latent_means[ , group_var]
+
+  # make a plot of the variance
+  variance_plot <-
+    ggplot(violindata, aes(x = 1, y = y, color = group, fill = group)) +
+    geom_split_violin() +
+    theme_void() +
+    theme(legend.position = "none") +
+    stat_summary(fun = "mean",
+                 geom = "crossbar",
+                 width = 0.5,
+                 colour = "black")
+
+  # make the plot with intercepts and slopes
+  intercept_plot <-
+    ggplot() +
+    # basic set up
+    theme_classic() +
+    xlab("Latent Variable") +
+    ylab("Observed Variable") +
+    coord_cartesian(xlim = x_limits, ylim = y_limits) +
+    # plot the intercepts
+    geom_point(data = pointdata,
+               aes(x = x, y = y, color = group),
+               inherit.aes = FALSE) +
+    geom_errorbar(data = pointdata,
+                  aes(x = x, ymin = ymin, ymax = ymax, color = group),
+                  inherit.aes = FALSE, width = .10) +
+    # plot the slopes
+    geom_abline(data = linedata,
+                aes(slope = slope, intercept = intercept, color = group)) +
+    geom_ribbon(data = ribbondata,
+                aes(x = x, ymin = ymin, ymax = ymax, fill = group),
+                inherit.aes = FALSE, alpha = .2) +
+    scale_color_discrete(name = "Group") +
+    scale_fill_discrete(name = "Group") +
+    geom_vline(xintercept = 0) +
+    theme(axis.line.y = element_blank())
+
+  # make the latent means plot
+  mean_plot <- ggplot(latent_means, aes(x = lv, fill = group)) +
+    geom_density(alpha = .2) +
+    theme_classic() +
+    xlab("Latent Variable") +
+    ylab("Density") +
+    geom_vline(data = latent_means %>% group_by(group) %>% summarize(mean = mean(lv)),
+               aes(xintercept = mean, color = group)) +
+    theme(legend.position = "none") +
+    coord_cartesian(xlim = x_limits)
+
+  y_range = abs(y_limits[2] - y_limits[1])
+
+  # line up the two plots
+  prow <- plot_grid(
+    intercept_plot +
+      ggtitle("Item Invariance") +
+      theme(legend.position = "none") +
+      annotation_custom(ggplotGrob(variance_plot),
+                        xmin = .25, xmax = 1,
+                        ymin = y_limits[1], ymax = y_limits[2]-y_range/1.8),
+    mean_plot +
+      ggtitle("Latent Mean Distribution") +
+      theme(legend.position = "none"),
+    align = 'vh',
+    hjust = -1,
+    nrow = 1
+  )
+
+  # get the legend
+  legend_b <- get_legend(
+    intercept_plot +
+      guides(color = guide_legend(nrow = 1)) +
+      theme(legend.position = "bottom")
+  )
+
+  # send out the plot
+  together <- plot_grid(prow, legend_b, ncol = 1, rel_heights = c(1, .1))
+
+  return(list("complete" = together,
+              "intercept" = intercept_plot,
+              "mean" = mean_plot,
+              "variance" = variance_plot))
+
+}

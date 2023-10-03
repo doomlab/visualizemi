1c99af184f4524c8df1b5f51a478827887fc9c34
The Doom Lab <buchananlab@gmail.com>
2023-10-02 22:18:42 -0500

    this is such a stupid hack

--- R/globals.R
+++ R/globals.R
@@ -5,3 +5,11 @@ intercept <- invariant <- lv <- model <- model_difference <- model_number <- n_b
 n_random <- non_invariant <- op <- random_1 <- random_2 <- random_difference <-
 random_fit <- random_index_difference <- random_non_invariant <- slope <- std.all <-
 std.error <- term <- type <- x <- y <- ymax <- ymin <- NULL
+
+library(ggplot2)
+library(lavaan)
+library(broom)
+library(cowplot)
+library(dplyr)
+library(tidyr)
+library(introdataviz)
